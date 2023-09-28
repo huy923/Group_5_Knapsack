53afa3487440233154e319110f0e7d91dce05be3
huy923 <huyxam233@gmail.com>
2023-09-28 22:18:09 +0700

    new def

--- file python/Algorithm.py
+++ file python/Algorithm.py
@@ -2,7 +2,8 @@ class Item:
     def __init__(self, value, cost):
         self.value = value
         self.cost = cost
-    
+
+
 def MarginalRatioThresholding(alpha, K, items, fs=0, costS=0):
     array = []
     for item in items:
@@ -13,11 +14,23 @@ def MarginalRatioThresholding(alpha, K, items, fs=0, costS=0):
             fs += item.value
     return array, fs, costS
 
-items = [Item(value=3, cost=2), Item(value=4, cost=3), Item(value=5, cost=4),Item(value=7,cost=5)]
-alpha = 0.5 # α ∈ (0, 1]
+
+items = [Item(value=3, cost=2), Item(value=4, cost=3),
+         Item(value=5, cost=4), Item(value=7, cost=5)]
+alpha = 0.5  # α ∈ (0, 1]
 K = 10 
 # Call the function and print the result
-result_array, result_fs, result_costS = MarginalRatioThresholding(alpha, K, items)
+result_array, result_fs, result_costS = MarginalRatioThresholding(
+    alpha, K, items)
+
 print("Array:", result_array)
 print("Total fs:", result_fs)
 print("Total costS:", result_costS)
+
+# def Singleton(items):
+#     best_item = max(items, key=lambda x: x.value)
+#     return best_item
+
+# Run the Singleton algorithm and print the result
+# singleton_set = Singleton(items)
+# print("Singleton Set:", [item.value for item in singleton_set])
